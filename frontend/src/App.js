530f831a0671b6947fb9711a9a323df69e4c8ede
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-06-13 18:58:05 +0530

    Add Router Configuration for the front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,37 @@
 import Navbar from "./components/Navbar/Navbar";
+import { useAuthContext } from "./context/AuthContext";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
+  const { authUser } = useAuthContext();
+  /* React Router Configuration */
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "signIn",
+          element: authUser ? <Navigate to="/" /> : <Login />,
+        },
+        {
+          path: "signUp",
+          element: authUser ? <Navigate to="/" /> : <Signup />,
+        },
+      ],
+    },
+  ]);
   return (
     <>
-      <Navbar />
+      {/* Passing all data router objects to this component to render the app and enable the rest of the data APIs */}
+      <RouterProvider router={router} />
     </>
   );
 }
